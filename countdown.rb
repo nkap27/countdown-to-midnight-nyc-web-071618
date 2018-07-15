3b9d74829a5a72f03f0f433495b497f4915a9ef3
Natasha K <kapr.natasha@gmail.com>
2018-07-15 17:02:10 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(number)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  while number >= 1
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep 1
+  end
+  return "HAPPY NEW YEAR!"
+end
